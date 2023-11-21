c2e9448acb0db23e2c949c452779c36b60c954ec
AnMal <junkie_@msn.com>
2023-11-21 08:09:09 +0100

    Add StoredProcedure to Messages table
    Add procedure for group messages
    Renamed procedure for direct messages

---
+++ PINChat.Data/pinchat/Stored Procedures/spMessages_GetByGroupId.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [PINChat].[spMessages_GetByGroupId]
+	@TargetId nvarchar(128)
+AS
+begin 
+	set nocount on;
+
+	SELECT 
+		m.Id,
+		m.SourceId,
+		m.TargetId,
+		m.Content,
+		m.[Image],
+		m.CreatedDate
+	FROM [PINChat].[Messages] m
+	WHERE 
+		TargetId = @TargetId
+		AND m.IsArchived = 0
+end
